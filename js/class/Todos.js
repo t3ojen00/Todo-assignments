26a80013294553bc1fe5cd69e85e3749a1b0a00c
enniemilia <130854866+enniemilia@users.noreply.github.com>
2024-03-25 20:03:28 +0200

    assignments

---
+++ js/class/Todos.js
@@ -0,0 +1,75 @@
+import { Task } from './Tasks.js';
+
+class Todos {
+    #tasks = []
+    #backend_url = ''
+
+    constructor(url) {
+        this.#backend_url = url
+    }
+//haetaan 
+    getTasks = () => {
+        return new Promise(async(resolve, reject) => {
+            fetch(this.#backend_url)
+            .then((response) => response.json())
+            .then((json) => {
+                this.#readJson(json)
+                resolve(this.#tasks)
+            },(error) => {
+                reject(error)
+            })
+        })
+    }
+//lisätään taskeja
+    #addToArray = (id,text) => {
+        const task = new Task(id,text)
+        this.#tasks.push(task)
+        return task
+    }
+
+    addTask = (text) => {
+        return new Promise(async(resolve, reject) => {
+            const json = JSON.stringify({description: text})
+            fetch(this.#backend_url + '/new',{
+                method: 'post',
+                headers: {'Content-Type':'application/json'},
+                body: json
+            })
+            .then((response) => response.json())
+            .then((json) => {
+                resolve(this.#addToArray(json.id,text))
+            }, (error) => {
+                reject(error)
+            })
+        })
+    }
+
+//poistetaan taskeja
+    #removeFromArray =(id) => {
+        const arrayWithoutRemoved = this.#tasks.filter(task => task.id !== id)
+        this.#tasks = arrayWithoutRemoved
+    }
+
+    removeTask = (id) => {
+        return new Promise(async(resolve, reject)=>{
+            fetch(this.#backend_url + '/delete/' + id,{
+                method: 'delete'
+            })
+            .then((response) => response.json())
+            .then((json)=>{
+                this.#removeFromArray(id)
+                resolve(json.id)
+            },(error)=>{
+                reject(error)
+            })
+        })
+    }
+
+    #readJson = (tasksAsJson) => {
+        tasksAsJson.forEach(node => {
+            const task = new Task(node.id,node.description)
+            this.#tasks.push(task)
+        })
+    }
+}
+export { Todos }
---
+++ server/sql.sql
@@ -0,0 +1,11 @@
+drop database if exists todo;
+create database todo;
+use todo;
+
+create table task (
+    id serial primary key,
+    description varchar(255) not null
+);
+
+insert into task (description) values ('My test task');
+insert into task (description) values ('My another test task');
